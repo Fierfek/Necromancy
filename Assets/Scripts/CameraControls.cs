90d74100f3e5eb757e68dfeb93da25a938519e59
Fierfek <geekssw@yahoo.com>
2018-06-20 18:01:23 -0700

    Pixel Snapping
    # Fixed tile seam bug by adding pixel snapping
    # Adjusted Quality and Time settings
    # Adjusted Input for snappier movement
    # Added Camera Follow (Needs Improvement)
    # Added Player Movement
    # Added Castle Walls
    # Added and invisible collision sprite

---
+++ Assets/Scripts/CameraControls.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControls : MonoBehaviour {
+	Camera camera;
+	public float cameraHeight = -10;
+	public Transform target;
+
+	// Use this for initialization
+	void Start () {
+		camera = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.Translate(target.position - transform.position);
+	}
+
+	public void setTarget(Transform newTarget) {
+		target = newTarget;
+	}
+}
---
+++ Assets/Scripts/Necromancer/PlayerInput.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+
+	public float speed = 1;
+	private float x, y;
+	private Rigidbody2D body;
+	private Vector2 direction;
+	Vector2 newPosition;
+
+	// Use this for initialization
+	void Start () {
+		body = GetComponent<Rigidbody2D>();
+		direction = Vector2.zero;
+		newPosition = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		x = Input.GetAxis("Horizontal");
+		y = Input.GetAxis("Vertical");
+
+		direction.Set(x, y);
+
+		body.MovePosition(body.position + (direction.normalized * speed) * Time.deltaTime);
+	}
+}
